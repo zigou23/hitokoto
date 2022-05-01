68bf6265e935ff57e9f603d13e3562bf57de602b
zigou23 <a1923458037@gmail.com>
2022-05-01 11:03:01 +0800

    deno

---
+++ deno/README.md
@@ -0,0 +1,29 @@
+# Hitokoto deno
+
+## 使用方法
+
+https://hitokoto.deno.dev
+
+| 参数   | 参数用途          |
+| ------ | ----------------- |
+| /js    | 打印参数          |
+| /json  | 返回json参数      |
+| /text  | 返回文本          |
+| ?`url` | 获取url返回值[^1] |
+
+### url请求内容格式
+
+一行一个一言，如果有from[^2]来源，请在一言后面加上`|`分隔线
+
+> 参考示例
+> ```
+> 所谓成长，就是拥有无论做什么，做不做得成，都不害怕的底气|from1|from2
+> 只要往着想要的生活前进，即使摔倒了，也能安慰自己起飞的过程|from1
+> 不要跟朋友合伙做生意,跟朋友做生意有风险|from1|from2
+> 能量的意义不在于正负，而在于让你前行|from1
+> ```
+
+
+
+[^1]: url 格式为`"https://cdn.jsdelivr.net/"+url+".txt"`中间的值，其他域名暂时未开放（防止滥用请求） 例如：https://hitokoto.deno.dev/json?url=gh/zigou23/hitokoto/my 
+[^2]: https://hitokoto.deno.dev/json?url=gh/zigou23/hitokoto/test
---
+++ deno/deno.tsx
@@ -0,0 +1,130 @@
+import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
+
+async function handleRequest(request: Request) {
+  const req = request
+  const urlStr = req.url
+  const urlObj = new URL(urlStr)
+  const path = urlObj.href.substr(urlObj.origin.length)
+  const headers_init = {
+    headers: {
+      "content-type": "application/javascript; charset=utf-8",  //默认UTF-8格式
+      "Access-Control-Allow-Origin": "*", //如果确定域名,把"*"改成"https://hitokoto.https://hitokoto.deno.dev"
+      // "Access-Control-Allow-Methods": "GET" 
+    }
+  }
+  const status_404 = {
+    status: 404,
+    headers: {
+      "content-type": "application/javascript; charset=utf-8",  //默认UTF-8格式
+      "Access-Control-Allow-Origin": "*",
+    }
+  }
+  console.log(path)
+  try{
+    if (path=="/favicon.ico") { //favicon
+      return fetch("https://cdn.jsdelivr.net/npm/zg-cdn/logo/64.png")
+    }
+    // 判断非法请求用户, 很简陋
+    const hostdomain = request.headers.get("host");
+    const ua = request.headers.get("user-agent");
+    if (hostdomain != 'hitokoto.deno.dev' || !ua.match(/Mozilla\/5.0\b/i) ) {
+      return new Response(JSON.stringify({'status':400,'msg':'illegal request'}), status_404);
+    }
+    /**
+     * Hitokoto
+     * /
+     */
+    if (path.startsWith('/')) {
+      const url = urlObj.searchParams.get('url'); //获取?url
+      let HitokotoFetchURL="";
+      // 判断?url=是否有数值
+      if(url){
+        HitokotoFetchURL="https://cdn.jsdelivr.net/"+url+".txt"
+      }else{ //没有使用默认值
+        HitokotoFetchURL="https://cdn.jsdelivr.net/gh/sy-records/hitokoto@master/hitokoto.txt"
+      }
+      /** 参考
+       * https://medium.com/deno-the-complete-reference/fetch-timeout-in-deno-91731bca80a1
+       * 请求限制时间
+       */
+      const c=new AbortController();
+      const id=setTimeout(() => c.abort(), 1000);
+      const r=await fetch(HitokotoFetchURL, {signal: c.signal});
+      clearTimeout(id);
+      //以"\n"回车为分界线储值
+      const Hitokotos = (await r.text()).split("\n")
+      if(Hitokotos.length < 5){
+        return new Response(JSON.stringify({'status':404,'msg':'rows less than 5'}), status_404);
+      }
+      const random = randomNum(0,Hitokotos.length-1) //生成随机数
+      let Hitokoto=Hitokotos[random]
+      // 获取其它信息每行"|"之后的信息留作from msg1
+      let from1 = ''
+      let from2 = ''
+      if(Hitokoto.split('|')){ //判断那一行是否有"|"有则返回内容
+        const msg = Hitokoto.split('|')
+        Hitokoto = msg[0] || ''
+        from1 = msg[1] || ''
+        from2 = msg[2] || ''
+      }
+      //js时返回的值
+      if (path.startsWith('/js')) {
+        if(path.startsWith('/json')){
+          return new Response(JSON.stringify({
+          "Hitokoto":Hitokoto,
+          "from":from1 || '',
+          "from2":from2 || '',
+          "lenth":Hitokoto.length,
+          "id":random+1,
+          "rows":Hitokotos.length,
+          "msg":"页面正在写了(咕咕咕"
+        }), headers_init)
+        }
+        return new Response("document.write('"+Hitokoto+"')", headers_init)
+      }
+      //纯文本返回的值
+      if (path.startsWith('/text')) {
+        return new Response(Hitokoto, headers_init)
+      }
+      return new Response(JSON.stringify({
+        "Hitokoto":Hitokoto,
+        "from":from1 || '',
+        "from2":from2 || '',
+        "lenth":Hitokoto.length,
+        "id":random+1,
+        "rows":Hitokotos.length,
+        "msg":"页面正在写了(咕咕咕"
+      }), headers_init)
+    }
+  }catch(err) {
+    console.log(err)
+    return new Response(JSON.stringify({'status':500,'msg':'Request timed out'}), {
+      status: 500,
+      headers: {
+        "content-type": "application/javascript; charset=utf-8",  //默认UTF-8格式
+        "Access-Control-Allow-Origin": "*",
+      }
+    })
+}
+  
+}
+// 随机数
+function randomNum(minNum,maxNum){
+  switch(arguments.length){
+    case 1:
+      return parseInt(Math.random()*minNum+1,10);
+    break;
+    case 2:
+      return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10);
+    break;
+      default:
+        return 0;
+      break;
+  }
+}
+console.log("Listening on http://localhost:8000");
+await serve(handleRequest);
+
+function ErrorPage(arg0: string): BodyInit {
+throw new Error("Function not implemented.");
+}
